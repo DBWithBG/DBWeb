0b08fc4963c5a12981c104a7a2d6dca4cb0bba67
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2018-04-11 17:29:24 +0200

    Template admin + login

---
+++ resources/views/admin/layout/head.blade.php
@@ -0,0 +1,28 @@
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Dosis:300" rel="stylesheet">
+
+<!-- favicon icon -->
+<link rel="shortcut icon" type="images/png" href="{!! asset("inbo/images/favicon.ico") !!}">
+
+<!-- all css here -->
+<link rel="stylesheet" href="style.css">
+<link type="text/css" rel="stylesheet" href="{{asset('inbo/css/bootstrap.min.css')}}" />
+<link type="text/css" rel="stylesheet" href="{{asset('inbo/css/animate.css')}}" />
+<link type="text/css" rel="stylesheet" href="{{asset('inbo/css/meanmenu.min.css')}}" />
+<link type="text/css" rel="stylesheet" href="{{asset('inbo/css/owl.carousel.css')}}" />
+<link type="text/css" rel="stylesheet" href="{{asset('inbo/lib/css/nivo-slider.css')}}" />
+<link type="text/css" rel="stylesheet" href="{{asset('inbo/lib/css/preview.css')}}" />
+<link type="text/css" rel="stylesheet" href="{{asset('inbo/css/jquery.simpleGallery.css')}}" />
+<link type="text/css" rel="stylesheet" href="{{asset('inbo/css/jquery.simpleLens.css')}}" />
+<link type="text/css" rel="stylesheet" href="{{asset('inbo/css/font-awesome.min.css')}}" />
+<link type="text/css" rel="stylesheet" href="{{asset('inbo/css/style.css')}}" />
+<link type="text/css" rel="stylesheet" href="{{asset('inbo/css/responsive.css')}}" />
+<link rel="stylesheet" href="{{asset('css/sweetalert.css')}}">
+<!-- modernizr css -->
+<script src="{{asset('inbo/js/vendor/jquery-1.12.0.min.js')}}"></script>
+<script src="{{ asset('inbo/js/vendor/modernizr-2.8.3.min.js')}}"></script>
