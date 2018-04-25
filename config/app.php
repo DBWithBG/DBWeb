15b98831de174a7318a92eb732da72f574f0bc90
Randy <randy.hocine@gmail.com>
2018-04-25 10:51:47 +0200

    mise en place du cors

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //provider CORS cross origin resource
+        Barryvdh\Cors\ServiceProvider::class,
+
     ],
 
     /*
