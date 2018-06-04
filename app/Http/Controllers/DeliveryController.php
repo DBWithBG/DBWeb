9639a339c912f73175933d9c828f6a783bfd74ed
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2018-06-04 10:48:40 +0200

    test

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -12,14 +12,13 @@ class DeliveryController extends Controller
 
         $request = $request->toArray();
         $start_position = Position::create($request['response']['start_position']);
-        return $start_position->id;
         $end_position = Position::create($request['response']['end_position']);
         //TODO Calcul du statut selon l'heure envoyée
         $request['response']['delivery']['status'] = 'En cours';
         //TODO Calcul du prix
         $request['response']['delivery']['price'] = 10.00;
         $request['response']['delivery']['start_position_id'] = $start_position->id ;
-        $request['response']['delivery']['start_position_id'] = $end_position->id ;
+        $request['response']['delivery']['end_position_id'] = $end_position->id ;
         $delivery = Delivery::create($request['response']['delivery']);
 
         return $delivery->id;
