9515ad54953edc8b622b5405afc529f861fdd3ac
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2018-06-04 11:03:21 +0200

    test

--- app/Delivery.php
+++ app/Delivery.php
@@ -16,7 +16,7 @@ class Delivery extends Model
      * @var array
      */
     protected $fillable = [
-        'name', 'surname', 'comment', 'price', 'created_at', 'updated_at', 'start_position_id', 'end_position_id', 'customer_id'
+        'name', 'surname', 'comment', 'price', 'created_at', 'updated_at', 'start_position_id', 'end_position_id', 'customer_id', 'status'
     ];
 
     public function startPosition()
