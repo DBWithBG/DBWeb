9db9e4404170253f736b5e4828748edeeb129911
Randy HOCINE <randy@sup.sarl>
2018-06-21 10:19:54 +0200

    debug ajout delivery

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -52,6 +52,7 @@ class HomeController extends Controller
     public static function checkCustomerMobile(Request $request){
         if(isset($request->mobile_token)){
             $u=User::where('mobile_token','=',$request->mobile_token)->first();
+            return json_encode($u);
             if(!$u)
                 throw new \Error('Pas d\'utilisateur trouvé :( ! ');
 
