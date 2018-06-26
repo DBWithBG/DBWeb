50dd5f96874507498bc881c5d95b66324793d26f
Randy HOCINE <randy@sup.sarl>
2018-06-26 17:15:35 +0200

    m

--- app/Http/Controllers/phone/MobileController.php
+++ app/Http/Controllers/phone/MobileController.php
@@ -166,7 +166,8 @@ class MobileController extends Controller
             $take->save();
             $del->update(['status'=>Config::get('constants.PRIS_EN_CHARGE')]);
             $res_id=$take->id;
-            NotificationController::sendNotification(array_push(NotificationController::notifyPriseEnCharge(),["tokens"=>$del->customer->user->notify_token]));
+            $token=$del->customer->user->notify_token;
+            NotificationController::sendNotification(array_push(NotificationController::notifyPriseEnCharge(),["tokens"=>$token]));
         }
         return json_encode($res_id);
     }
