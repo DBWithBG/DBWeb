24f5f18b5e4f9be6a2670ea654385a71a6683830
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2018-07-09 09:26:34 +0200

    Socialite

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialController extends Controller
