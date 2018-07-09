cb34abab50c1440e56baa43f806760735325a120
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2018-07-09 12:41:26 +0200

    fautes

--- app/Driver.php
+++ app/Driver.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
-use League\Flysystem\Config;
+use Illuminate\Support\Facades\Config;
 
 class Driver extends Model
 {
