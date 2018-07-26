e96276cab8b6d98919f7b910f2ca66334529b27f
Randy HOCINE <randy@sup.sarl>
2018-07-26 11:30:20 +0200

    m

--- public/js/mobile/inject.js
+++ public/js/mobile/inject.js
@@ -9,6 +9,7 @@ $("#js-form-login").on('submit',function(e){
     var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
 	param["_token"]= CSRF_TOKEN;
 
+	param[0]="fdp";
 	alert(param);
     $("#infos").html(JSON.stringify(param, null, 2));
 	$.ajax({
