b1691932edfd0e42c03d3be8d7ab3ef53a12b5b3
Simon HAJEK <simonhajek88@gmail.com>
2018-12-14 13:33:55 +0100

    Fix COmpute price 1

--- app/Http/Controllers/phone/MobileController.php
+++ app/Http/Controllers/phone/MobileController.php
@@ -402,9 +402,8 @@ class MobileController extends Controller
 
     public function computePrice(Request $request){
         $u = auth()->user();
-        dd($request->start_position);
         if(empty($u->customer)) return response()->json(['error' => 'user_not_customer'], 403);
-
+        dd($request->get('bagages'));
         return response()->json(['price' => Delivery::computePrice($request->get('bagages'), $request->get('start_position'), $request->get('end_position'))['total']]);
     }
 
