0ef737621397e47a40b2410550889d15de7f5675
Simon HAJEK <simonhajek88@gmail.com>
2018-12-27 15:32:49 +0100

    Setup sociales conn

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -23,7 +23,6 @@ class SocialController extends Controller
      * Fonction qui va se charger de rediriger notre application vers l'url du provider
      */
     public function redirect($provider){
-        dd("redirect");
         return Socialite::driver($provider)->redirect();
     }
 
@@ -34,7 +33,6 @@ class SocialController extends Controller
      * Fonction de callback ou le provider nous redirige en passant l'utilisateur
      */
     public function callback($provider){
-        dd("callback");
         //Récupération de l'utilisateur renvoyé
         try{
             $providerUser = Socialite::driver($provider)->user();
