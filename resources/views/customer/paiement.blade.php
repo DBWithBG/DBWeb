5a75a9f188dc7abf54d1aa5e867982f21c20400c
Simon HAJEK <simonhajek88@gmail.com>
2019-02-14 12:20:50 +0100

    paiement process

--- resources/views/customer/paiement.blade.php
+++ resources/views/customer/paiement.blade.php
@@ -1,7 +1,7 @@
 @extends('customer.layouts.app')
 
 @section('content')
-    <form method="post" action="{{"delivery/paiement"}}" id="paiement">
+    <form method="post" action="{{"/delivery/paiement"}}" id="paiement">
         <div class="form-body">
             <input type="hidden" value="{{$delivery->id}}" name="delivery_id">
             Le montant total de votre commande est de {{$delivery->price}}
