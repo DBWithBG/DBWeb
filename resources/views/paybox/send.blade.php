c32d2303f3d209e015a44d15eaebf2a0d99241f6
Simon HAJEK <simonhajek88@gmail.com>
2019-02-14 13:48:20 +0100

    paiement process

---
+++ resources/views/paybox/send.blade.php
@@ -0,0 +1,9 @@
+    <form method="post" action="{{ $url }}" id="paybox-payment">
+        @foreach ($parameters as $name => $value)
+            <input type="hidden" name="{{ $name }}" value="{{ $value }}">
+        @endforeach
+
+        <input type="submit" value="Send payment">
+    </form>
+    <script>
+        document.getElementById("paybox-payment").submit();
