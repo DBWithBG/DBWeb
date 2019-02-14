ba54d79748daba76b69c5c4399fd61c793822315
Simon HAJEK <simonhajek88@gmail.com>
2019-02-14 12:23:39 +0100

    paiement process

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -139,8 +139,7 @@
             if(empty($delivery->id)){
                 return "ERROR_DELIVERY_NOT_FOUND";
             }
-            dd(Auth::user()->id, $delivery->user->id);
-            if(Auth::user()->id != $delivery->user_id){
+            if(Auth::user()->id != $delivery->customer->user->id){
                 return "OPERATION_NOT_ALLOWED";
             }
 
