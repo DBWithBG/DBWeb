f7a4ba3bbc9ff93d53d00729cddb546952dfa9d3
Simon HAJEK <simonhajek88@gmail.com>
2019-02-16 16:02:21 +0100

    Paiement

--- resources/views/customer/paybox/refused.blade.php
+++ resources/views/customer/paybox/refused.blade.php
@@ -3,9 +3,11 @@
 @section('content')
     <section class="section sec-padding" style="min-height: 80vh">
         <div class="section-title">
-            <div class="container">
-                Le paiement à été refusé<br>
-                <a href="">Effectuer une nouvelle tentative de paiement</a>
+            <div class="container text-center">
+                <h2>
+                    Le paiement à été refusé<br>
+                    <a href="">Effectuer une nouvelle tentative de paiement</a>
+                </h2>
             </div>
         </div>
     </section>
