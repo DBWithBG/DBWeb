b9c30d7633a56a4d1b505051a1947d5de66b062e
Simon HAJEK <simonhajek88@gmail.com>
2019-02-16 17:27:50 +0100

    Update facture

--- resources/views/customer/paybox/accepted.blade.php
+++ resources/views/customer/paybox/accepted.blade.php
@@ -3,9 +3,11 @@
 @section('content')
     <section class="section sec-padding">
         <div class="section-title">
-            <div class="container">
+            <div class="container" style="text-align: center">
                 <h1>Nous vous confirmons votre paiement, vous recevrez la facture par email dans quelques minutes.</h1>
                 <a class="btn btn-primary btn-info" href="{{url("delivery/".$delivery->id."/showFacture")}}">Télécharger ma facture</a>
+                <p>Nous nous occupons de vos bagages. Nous vous tiendrons au courant de l'avancée de votre commande.<br>
+                 Merci de votre confiance, à bientôt.</p>
             </div>
         </div>
     </section>
