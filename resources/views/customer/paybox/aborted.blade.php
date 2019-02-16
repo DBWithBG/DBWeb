0eaa16e099e260a0cd217412c3136a6426d73cb2
Simon HAJEK <simonhajek88@gmail.com>
2019-02-16 16:18:48 +0100

    Paiement

--- resources/views/customer/paybox/aborted.blade.php
+++ resources/views/customer/paybox/aborted.blade.php
@@ -7,6 +7,7 @@
                     <br><br>
                     <h1>Vous avez abandonné le paiement</h1><br>
                     <form action="{{url("/delivery/paiement")}}" method="POST">
+                        {{csrf_field()}}
                         <input type="hidden" value="{{$delivery->id}}" name="delivery_id">
                         <button type="submit" class="btn btn-primary btn-success">Accéder au paiement de {{$delivery->price}} €</button>
                     </form>
