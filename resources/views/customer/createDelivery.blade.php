c024c793f9f82d6fa7cb668c09e65481274e0822
Simon HAJEK <simonhajek88@gmail.com>
2019-02-16 15:09:32 +0100

    Paiement

--- resources/views/customer/createDelivery.blade.php
+++ resources/views/customer/createDelivery.blade.php
@@ -222,8 +222,8 @@
             $('#checkbox-course-retour').on('click', function () {
                 console.log('plop');
                 if($(this).is(':checked')) {
-                    console.log(moment($('#datetimepicker4').val()));
-                    $('#retour-datetimepicker').data("DateTimePicker").minDate(moment($('#datetimepicker4').val()));
+                    console.log(new Date($('#datetimepicker4').val()));
+                    $('#retour-datetimepicker').data("DateTimePicker").minDate(moment(new Date($('#datetimepicker4').val())));
                     $('#retour-datetimepicker').data("DateTimePicker").maxDate(moment().add(1, 'years'));
                     $('#row-date-course-retour').show();
                 } else {
