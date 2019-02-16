8d05336318dfdc32b0efa200afa4896e4691da07
Simon HAJEK <simonhajek88@gmail.com>
2019-02-16 16:59:18 +0100

    Paiement success

--- app/Http/Controllers/FactureController.php
+++ app/Http/Controllers/FactureController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Delivery;
 use App\Driver;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use niklasravnsborg\LaravelPdf\Facades\Pdf;
 use Illuminate\Support\Facades\Config;
 
