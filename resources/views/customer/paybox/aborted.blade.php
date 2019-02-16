493fc002bf596fa93975e4ebf61ccdca1b285b53
Simon HAJEK <simonhajek88@gmail.com>
2019-02-16 16:17:25 +0100

    Paiement

--- resources/views/customer/paybox/aborted.blade.php
+++ resources/views/customer/paybox/aborted.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <section class="section sec-padding">
         <div class="section-title">
-            <div class="container">
+            <div class="container" style="text-align: center">
                     <br><br>
-                    Vous avez abandonné le paiement<br>
+                    <h1>Vous avez abandonné le paiement</h1><br>
                     <form action="{{url("/delivery/paiement")}}" method="POST">
                         <input type="hidden" value="{{$delivery->id}}" name="delivery_id">
                         <button type="submit" class="btn btn-primary btn-success">Accéder au paiement de {{$delivery->price}} €</button>
