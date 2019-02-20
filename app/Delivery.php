29a9b35590d844877495d597ad45ce3eadfe676a
Simon HAJEK <simonhajek88@gmail.com>
2019-02-20 12:37:07 +0100

    delete delivery

--- app/Delivery.php
+++ app/Delivery.php
@@ -127,7 +127,7 @@ class Delivery extends Model
         // =(3+2*RACINE(B14)*(1*RACINE($A$2)))*1,2
 
         $priceLine = Price::where('bags_min', '<=', $nb_bags)->where('bags_max', '>=', $nb_bags)
-            ->where('promotion', '1')->first();
+            ->where('promotion', '1')->where('end_date', '<=', $start_date)->first();
         if(empty($priceLine)){
             $priceLine = Price::where('bags_min', '<=', $nb_bags)->where('bags_max', '>=', $nb_bags)
                 ->where('promotion', '0')->first();
