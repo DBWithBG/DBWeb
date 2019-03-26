34bf42c3d03f6587fc461196b5df8cbf98afe204
Simon HAJEK <simonhajek88@gmail.com>
2019-03-26 21:15:45 +0100

    Train

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -92,8 +92,9 @@
                     <div class="smart-wrap" style="min-height: 140px">
                         <div class="smart-forms">
 
-                            <p>Vous pouvez entrer ici votre numéro de train (généralement 4 chiffres) il vous sera ensuite proposer les gares compatibles avec Deliverbag.
-                                Vous pourez ensuite choisir la date de départ de votre train.
+                            <p>Vous pouvez entrer ici votre numéro de train (généralement 4 chiffres).<br>
+                                Vous pourez ensuite choisir la date de départ de votre train.<br>
+                                Enfin, il vous sera ensuite proposer les gares compatibles avec Deliverbag
                                 <br>Cela pré-remplira automatiquement votre adresse d'arrivée et <strong>nous serons au courant en cas de retard</strong>
                             </p>
                             <div class="form-body">
