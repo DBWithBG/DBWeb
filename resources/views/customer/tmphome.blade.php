bdce9d195b64049ccc31ae58cac8ac36d1530351
Simon HAJEK <simonhajek88@gmail.com>
2019-04-11 13:06:11 +0200

    No tel

--- resources/views/customer/tmphome.blade.php
+++ resources/views/customer/tmphome.blade.php
@@ -13,7 +13,7 @@
 <article>
     <h1>Nous revenons rapidement</h1>
     <div>
-        <p>Nous sommes désolé de cette déconvenue. <br>En attendant, vous pouvez utiliser nos services en nous contactant directement<br>Vous pouvez nous contacter à <a href="mailto:#">bordeaux@deliverbag.com</a>, ou au +33(0)5.57.84.01.11 !</p>
+        <p>Nous sommes désolé de cette déconvenue. <br>En attendant, vous pouvez utiliser nos services en nous contactant directement<br>Vous pouvez nous contacter à <a href="mailto:#">bordeaux@deliverbag.com</a>, ou au +33(0)5.57.87.01.11 !</p>
         <p>&mdash; L'équipe Deliverbag</p>
     </div>
 </article>
