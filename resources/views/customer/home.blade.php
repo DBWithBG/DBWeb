b1a2c3c8410cb2058162ee75c9603c1c0ea4346a
Simon HAJEK <simonhajek88@gmail.com>
2019-04-15 23:04:41 +0200

    Update remove train

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -26,9 +26,9 @@
                                         {{csrf_field()}}
 
                                         <div class="form-body">
-                                            <!-- <div class="row">
+                                             <div class="row">
                                                 <span class="voyage-train">Laissez vous guider</span>
-                                               <div class="modal-buttons">
+                                              <!-- <div class="modal-buttons">
                                                     <label onclick="openTrainModal()"
                                                            class="button btn-primary"><i
                                                                 class="fa fa-train"></i></label>-->
@@ -39,7 +39,7 @@
                                             </div>-->
                                             <div class="row">
 
-                                                <label class="field prepend-icon" style="margin-top: 10px">
+                                                <label class="field prepend-icon" style="margin-top: 0px">
 
                                                     <input id="adresse_input_depart" type="search"
                                                            class="gui-input"
