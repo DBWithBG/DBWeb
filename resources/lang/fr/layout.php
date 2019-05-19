73d4296de89dbf260b33918d7771393cbe2ecf74
Simon HAJEK <simonhajek88@gmail.com>
2019-05-19 14:16:41 +0200

    tests languages

--- resources/lang/fr/layout.php
+++ resources/lang/fr/layout.php
@@ -1,5 +1,38 @@
 <?php
 
 return [
-    'apropos' => 'À propos de nous'
+    // footer
+    'aproposnous' => 'À propos de nous',
+    'apropos' => 'À propos',
+    'mentionslegales' => 'Mentions légales',
+    'regles' => 'Règles',
+    'aide' => 'Aide',
+    'decouvrir' => 'Découvrir',
+    'confiance' => 'Confiance',
+    'securite' => 'Sécurité',
+    'partenaires' => 'Partenaires',
+    'accespro' => 'Accès professionnel',
+    'tgc' => 'Termes générales et conditions',
+    // RIGHT MENU
+    'liensutiles' => 'Liens utiles',
+    'historiquecourses' => 'Historique de vos courses',
+    'vosbagages' => 'Vos bagages',
+    'modifieremail' => 'Modifier votre adresse email',
+    'modifiermdp' => 'Modifier votre mot de passe',
+    'nouscontacter' => 'Nous contacter',
+    // top bar
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
 ];
--- resources/views/customer/layouts/topBarre.blade.php
+++ resources/views/customer/layouts/topBarre.blade.php
@@ -28,6 +28,10 @@
                                     @else
                                         <li class="right"><a href="{{url("connexion")}}">Connexion</a></li>
                                     @endif
+                                    <ul class="languagepicker">
+                                        <a href="{{url('language/fr')}}"><li><img src="http://i65.tinypic.com/300b30k.png"/>Français</li></a>
+                                        <a href="{{url('language/en')}}"><li><img src="http://i64.tinypic.com/fd60km.png"/>English</li></a>
+                                    </ul>
                                 </ul>
                             </div>
                         </div>
