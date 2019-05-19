d017f32a79d865b2273c982b840824de6f2d863d
Simon HAJEK <simonhajek88@gmail.com>
2019-05-19 14:27:25 +0200

    dropdown languages

--- resources/views/customer/layouts/topBarre.blade.php
+++ resources/views/customer/layouts/topBarre.blade.php
@@ -91,12 +91,10 @@
                                     @else
                                         <li class="right"><a href="{{url("connexion")}}">Connexion</a></li>
                                     @endif
-                                    <li style="margin-top: 10px">
-                                        <ul class="languagepicker">
+                                        <ul class="languagepicker" style="margin-top: 10px">
                                             <a href="{{url('language/fr')}}"><li><img src="http://i65.tinypic.com/300b30k.png"/> Français</li></a>
                                             <a href="{{url('language/en')}}"><li><img src="http://i64.tinypic.com/fd60km.png"/> English</li></a>
                                         </ul>
-                                    </li>
                                 </ul>
                             </div>
                         </div>
