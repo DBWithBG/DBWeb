bd1d8aba38564c9a3e908bafdb695fa5dc771fde
Simon HAJEK <simonhajek88@gmail.com>
2019-05-21 10:09:16 +0200

    fix area

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -401,14 +401,10 @@
         function verifyDepartment(place) {
             //var bdx_metropole = {33130, 33370 ,33110,33170,33700,33185,33530,33127,33400,33810,33290,33150,33520,33160,33310,33440,33270,33140,33560,33600,33320,33800,33100,33000,33200,33300};
             var res = place.address_components;
-            console.log(res)
             var found = false;
             for(i=0; i< res.length; i++) {
-                console.log(res[i].types)
                 if(res[i].types.includes("administrative_area_level_2")) {
                     for (var k = 0; k < departments.length; k++) {
-                        console.log(departments[k].number);
-                        console.log(res[i].long_name, departments[k].name)
                         if (res[i].long_name == departments[k].name) {
                             found = true;//On a trouvé une correspondance
                             break;
