6d6996d0f15f2699c27d06f4a9ff38056acdc62f
Simon HAJEK <simonhajek88@gmail.com>
2019-05-21 23:24:33 +0200

    flags

--- resources/views/customer/createDelivery.blade.php
+++ resources/views/customer/createDelivery.blade.php
@@ -239,18 +239,15 @@
             }
 
             $('#retour-datetimepicker').datetimepicker({
-                regional: '{{\Illuminate\Support\Facades\App::getLocale()}}',
                 defaultDate: moment()
             });
 
             $('#datetimepicker4').datetimepicker({
-                regional: '{{\Illuminate\Support\Facades\App::getLocale()}}',
                 defaultDate: moment()
             });
             $('#datetimepicker5').datetimepicker({
-                regional: '{{\Illuminate\Support\Facades\App::getLocale()}}',
                     defaultDate: moment().add(2, 'hours')
-            });
+            });$.datepicker.setDefaults( $.datepicker.regional[ "{{\Illuminate\Support\Facades\App::getLocale()}}" ] );
             $('#datetimepicker4').data("DateTimePicker").minDate(moment().add(2, 'hours'));
             $('#datetimepicker4').data("DateTimePicker").maxDate(moment().add(1, 'years'));
                 $('#datetimepicker5').data("DateTimePicker").minDate(moment().add(4, 'hours'));
