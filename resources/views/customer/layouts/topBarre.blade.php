8555ca8fbe60d0e0b32138d091936f283163cde6
Simon HAJEK <simonhajek88@gmail.com>
2019-05-21 22:53:29 +0200

    flags

--- resources/views/customer/layouts/topBarre.blade.php
+++ resources/views/customer/layouts/topBarre.blade.php
@@ -91,7 +91,7 @@
                                     @else
                                         <li class="right"><a href="{{url("connexion")}}">{{trans('layout.connexion')}}</a></li>
                                     @endif
-                                    <ul class="languagepicker" style="margin-top: 20px;opacity: 0.6;background-color: transparent ">
+                                    <ul class="languagepicker" style="margin-top: 20px;background-color: transparent;color: white">
                                         @if(\Illuminate\Support\Facades\App::isLocale('fr'))
                                             <a  href="{{url('language/fr')}}"><li><img src="{!! asset("img/minifr.png") !!}"/>{{trans('layout.francais')}}</li></a>
                                             <a href="{{url('language/en')}}"><li><img src="{!! asset("img/minien.png") !!}"/>{{trans('layout.anglais')}}</li></a>
