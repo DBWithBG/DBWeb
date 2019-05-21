c147fea9a1468d40b082f98861e386d097c2ddeb
Simon HAJEK <simonhajek88@gmail.com>
2019-05-21 22:42:56 +0200

    Trad in english

--- resources/views/customer/layouts/topBarre.blade.php
+++ resources/views/customer/layouts/topBarre.blade.php
@@ -91,13 +91,13 @@
                                     @else
                                         <li class="right"><a href="{{url("connexion")}}">{{trans('layout.connexion')}}</a></li>
                                     @endif
-                                    <ul class="languagepicker" style="margin-top: 20px;opacity: 0.2">
+                                    <ul class="languagepicker" style="margin-top: 20px;opacity: 0.6;background-color: transparent ">
                                         @if(\Illuminate\Support\Facades\App::isLocale('fr'))
-                                            <a style="opacity: 0.2" href="{{url('language/fr')}}"><li><img src="http://i65.tinypic.com/300b30k.png"/>{{trans('layout.francais')}}</li></a>
-                                            <a style="opacity: 0.2" href="{{url('language/en')}}"><li><img src="http://i64.tinypic.com/fd60km.png"/>{{trans('layout.anglais')}}</li></a>
+                                            <a  href="{{url('language/fr')}}"><li><img src="http://i65.tinypic.com/300b30k.png"/>{{trans('layout.francais')}}</li></a>
+                                            <a href="{{url('language/en')}}"><li><img src="http://i64.tinypic.com/fd60km.png"/>{{trans('layout.anglais')}}</li></a>
                                         @else
-                                            <a style="opacity: 0.2" href="{{url('language/en')}}"><li><img src="http://i64.tinypic.com/fd60km.png"/>{{trans('layout.anglais')}}</li></a>
-                                            <a style="opacity: 0.2" href="{{url('language/fr')}}"><li><img src="http://i65.tinypic.com/300b30k.png"/> {{trans('layout.francais')}}</li></a>
+                                            <a href="{{url('language/en')}}"><li><img src="http://i64.tinypic.com/fd60km.png"/>{{trans('layout.anglais')}}</li></a>
+                                            <a href="{{url('language/fr')}}"><li><img src="http://i65.tinypic.com/300b30k.png"/> {{trans('layout.francais')}}</li></a>
                                         @endif
                                     </ul>
                                 </ul>
