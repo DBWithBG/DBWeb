2e85508a4bc427c8709de5a9a53f4b9696226e3b
Simon HAJEK <simonhajek88@gmail.com>
2019-05-21 23:43:22 +0200

    flags

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -127,12 +127,12 @@
             $delivery = Delivery::find($request['delivery_id']);
 
                 // EN 05/22/2019 3:32 AM
-            $delivery->end_date = Carbon::createFromFormat('m/d/Y h:m A', $request['datetimeend']);
+            $delivery->end_date = Carbon::createFromFormat('mm/dd/Y h:m A', $request['datetimeend']);
 
             if(empty($request['bagages'])){
                 throw new \Error('Please enter a least a bag');
             }
-            $start_date = Carbon::createFromFormat('m/d/Y h:m A', $request['datetimevalue']);
+            $start_date = Carbon::createFromFormat('mm/dd/Y h:m A', $request['datetimevalue']);
             //$start_date = Carbon::createFromFormat('Y-m-j',$request['date_prise_en_charge']);
             //$start_date->setTimeFromTimeString($request['time_prise_en_charge']);
             $request['time_consigne'] = null;
