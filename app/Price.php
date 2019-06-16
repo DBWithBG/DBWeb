5cb8fd6bb715005f3dd11f69ffdcf6faf0742186
Simon HAJEK <simonhajek88@gmail.com>
2019-06-16 20:27:30 +0200

    Prices

--- app/Price.php
+++ app/Price.php
@@ -17,7 +17,8 @@ class Price extends Model
     protected $fillable = [
         'createur', 'bags_min', 'bags_max', 'to_add_driver', 'coef_kilometers_driver', 'coef_bags_driver',
         'price_per_bag', 'price_ret_per_bag',
-        'coef_total_driver', 'coef_total_driver', 'coef_deliver', 'start_date', 'end_date', 'promotion', 'to_add_retour'
+        'coef_total_driver', 'coef_total_driver', 'coef_deliver', 'start_date', 'end_date', 'promotion', 'to_add_retour',
+        'postal_codes', 'price_hors_postal'
     ];
     public $timestamps = true;
 }
