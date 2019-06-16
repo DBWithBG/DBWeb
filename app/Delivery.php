961ee5ff0f696836ebdf394041d78989d2158335
Simon HAJEK <simonhajek88@gmail.com>
2019-06-16 20:51:19 +0200

    Prices

--- app/Delivery.php
+++ app/Delivery.php
@@ -133,10 +133,15 @@ class Delivery extends Model
                 ->where('promotion', '0')->first();
         }
 
+
         //$remuneration_driver = round(($priceLine->to_add_driver + $priceLine->coef_kilometers_driver * sqrt($distance)*($priceLine->coef_bags_driver * sqrt($priceLine->bags_min))) * $priceLine->coef_total_driver, 2);
         $remuneration_driver = 0;
         //$remuneration_deliver = round($remuneration_driver * $priceLine->coef_deliver*$nb_bags, 2);
-        $remuneration_deliver = round($priceLine->price_per_bag*$nb_bags,2);
+        if(!strpos($priceLine->postal_codes, $start_position->postal_code) && !strpos($priceLine->postal_codes, $end_position->postal_code)) {
+            $remuneration_deliver = round($priceLine->price_per_bag*$nb_bags,2);
+        }else {
+            $remuneration_deliver = round($priceLine->price_hors_postal*$nb_bags,2);
+        }
         //if($retour) $total = round($remuneration_driver + $remuneration_deliver + $priceLine->to_add_retour, 2);
         if($retour) {
             $total = round($remuneration_deliver + ($priceLine->price_ret_per_bag * $nb_bags), 2);
